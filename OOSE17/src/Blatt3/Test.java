6581571e024b3ed63d09eb23ec2eb061e73847dc
cronax3 <cronax3@192.168.0.115>
2017-05-13 10:16:15 +0200

    Verbesserungen an den Kommentaren

--- OOSE17/src/Blatt3/Test.java
+++ OOSE17/src/Blatt3/Test.java
@@ -8,11 +8,11 @@ public class Test {
 		((C2) a).f1();
 		((C1) b).f1();	
 		System.out.println(a.s);
-		b.f1();
-		b.f2();
-		b.f3();
+		b.f1(); // Gibt C1::f1 aus, weil die Funktion f1() die Funktion f1() von C1 überschreibt
+		b.f2(); //Gibt C1::f1 aus, weil C2 die Funktion f2() von C1 erbt und nicht überschreibt
+		b.f3(); //Gibt C2::f3 aus, weil C2 f3() als erweiterte Methode besitzt
 		//C1 c = new C1(); c.f3(); c.f3() FEHLER! f3 ist nicht in C1 enthalten, wird bei C2 als Erweiterung implementiert
-		C1 c = new C1(); ((C2) c).f1();
+		//C1 c = new C1(); ((C2) c).f1(); //C1 kann nicht in C2 typcastet werden
 		
 		}
 	
