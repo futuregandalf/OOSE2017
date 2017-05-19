a560599739590dcee51133d193c6bb459d003518
futuregandalf <schlingmann.s@gmx.de>
2017-05-19 16:35:17 +0200

    Add files via upload

---
+++ OOSE17/src/Point.java
@@ -0,0 +1,41 @@
+
+public class Point {
+	int x,y;
+	
+	public Point(int a, int b){
+	
+		x = a;
+		y = b;
+		
+	}
+	
+	public static double distance(Point a, Point b){
+		
+		double d;
+		
+		d = Math.sqrt(Math.pow((a.y)-(b.y), 2)+Math.pow((a.x)-(b.x), 2));
+		
+		
+		System.out.println(d);
+		return  d;
+		
+	}
+	
+	public static void ausgabe(Point a, Point b){
+		
+		System.out.println("Die Koordinaten sind a:(" +a.x +"|"+a.y +") und b:("+b.x +"|"+b.y+")");
+			
+	}
+	
+	public static void main(String[] args)
+	{
+		Point a = new Point(12,13);
+		Point b = new Point(15,16);
+		distance (a,b);
+		ausgabe (a,b);
+		
+	}
+	
+	
+	
+}
